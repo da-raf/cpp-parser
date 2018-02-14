d172ade9180b9e73308965fed1e2d989d4d2fd78
Raphael Dümig <duemig@in.tum.de>
2018-02-14 17:23:43 +0100

    various improvements to class listing example
    1. use command line arguments
    2. only parse definitions and no declarations

--- examples/list_class_defs.py
+++ examples/list_class_defs.py
@@ -1,28 +1,34 @@
 import sys; sys.path.append('..')
 
-import os.path
+import os
 
 import cpp_printer
 import cpp_parser
 import cpp_lang
 
-fg_dir = '/home/farad/Ohjelmoiminen/fg/flightgear'
+import argparse
+
+parser = argparse.ArgumentParser(
+        description='list class definitions in a source file'
+)
+parser.add_argument('source_file')
+args = parser.parse_args()
 
 # load source file
-source_code = '\n'.join(
-    open(os.path.join(fg_dir, 'src/Input/FGEventInput.hxx'))
-)
+source_code = '\n'.join( open(args.source_file) )
 
 # remove comments and preprocessor directives from the code
 stripped_source = (cpp_parser.comment | cpp_parser.preprocessor).suppress().transformString(source_code)
 
 printer = cpp_printer.CppPrinter()
-classes = (cpp_parser.hierarchical_type_def | cpp_parser.hierarchical_type_decl).searchString(stripped_source)
+classes = (cpp_parser.hierarchical_type_def).searchString(stripped_source)
 
 print('found %d classes:' % len(classes))
 
-for cl in classes:
-    print()
+for (i, cl) in enumerate(classes):
+    print('-------------------')
+    print('--- %2d ------------' % i)
+    print('-------------------')
     if type(cl[0]) == cpp_lang.CppHierarchicalTypeDefinition:
         print(printer.hierarchical_type_str(cl[0]))
 
